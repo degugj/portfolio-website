5ff0bd8d290a7e3b6965c2b635cb8864d3d9b08b
jackd <jdeguglielmo@umass.edu>
2020-12-20 23:10:21 -0500

    Updates to sidebar menu and dropdowns. Issues with highlighting area.

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,14 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  } 
+var coll = document.getElementsByClassName("nav-item");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
